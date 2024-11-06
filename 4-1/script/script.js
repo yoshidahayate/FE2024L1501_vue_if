f461631f780dfa46c1e2580122937e4de539eb23
yoshidahayate <2301215@s.asojuku.ac.jp>
2024-11-06 11:38:29 +0900

    Initial commit

---
+++ 4-1/script/script.js
@@ -0,0 +1,17 @@
+// JavaScript用ファイル
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            count: 1
+        };
+    },
+    methods: {
+        increment() {
+            this.count ++;
+        },
+        decrement() {
+            this.count --;
+        }
+    }
+});
